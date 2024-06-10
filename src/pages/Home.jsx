6e6f3b867455e6e8a49742055c47a86db1ff9fd1
Giovanny Jardim <gigiojardim11@gmail.com>
2024-06-10 06:15:44 -0300

    style: caard no centreo da tela

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,25 @@
+import { ButtonContainer, CardContainer, IconContainer } from "./Home.styles";
+
+import iconStaring from "../assets/icon-star.svg"
+
 export function Home() {
   return (
-    <div>
-      home
-    </div>
+    <CardContainer>
+        <IconContainer>
+          <img src={iconStaring} alt="" />
+        </IconContainer>
+
+        <h1>como foi o atendimento?</h1>
+        <p>conte nos blah blah blah</p>
+
+        <ButtonContainer>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>4</button>
+          <button>5</button>
+        </ButtonContainer>
+        <button>enviar</button>
+    </CardContainer>
   )
 }
